c0ae700ff0d5603372bd94c3fe230c6f846e4af8
Lovepreet <sin11278@sheridancollege.ca>
2022-02-24 21:52:41 -0500

    I'm done!

---
+++ src/javaapplication25/JavaApplication25.java
@@ -0,0 +1,18 @@
+
+
+package javaapplication25;
+
+/**
+ * This class +++ Insert class description here +++ 
+ * @author Lovepreet Singh
+ */
+public class JavaApplication25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}

db1a19fc2ca8274736872a872cab5a6ac5b93e73
Lovepreet <sin11278@sheridancollege.ca>
2022-02-24 22:00:18 -0500

    Revert "I'm done!"
    This reverts commit ab15cdd71504fdbd0ece8945ccdd47d0f7b33ca7.

--- src/javaapplication25/JavaApplication25.java
+++ src/javaapplication25/JavaApplication25.java
@@ -10,10 +10,6 @@ public class JavaApplication25 {
 
     /**
      * @param args the command line arguments
-     * 
-     * 
-     * 
-     * this is what 
      */
     public static void main(String[] args) {
         // TODO code application logic here
